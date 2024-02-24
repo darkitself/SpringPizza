ed35bd8a0dcf68a3e3c11aa86675d6c001db247e
MrLii <evgeniymrlii@gmail.com>
2024-02-24 13:59:59 +0500

    Initial commit

---
+++ src/main/java/com/example/springpizza/SpringPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPizzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springpizza/SpringPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
